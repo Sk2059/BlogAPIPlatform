58496398d0cff614f7ef2bbdb1632e4cdaf33d73
Sabinam Mahato <sksingh4919@gmail.com>
2026-07-25 12:56:31 +0545

    Phase 4: add user profile schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,5 @@
 from datetime import datetime 
-from pydantic import BaseModel , EmailStr
+from pydantic import BaseModel , EmailStr , ConfigDict,HttpUrl,Field
 
 from app.models.enums import UserRole
 
@@ -10,12 +10,63 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password:str
 
-class UserResponse(UserBase):
-    id :int 
-    role : UserRole
-    is_varified : bool
-    created_at : datetime
+class UserResponse(BaseModel):
 
-    model_config ={
-        "from_attributes": True    
-        }
+    id: int
+    email: EmailStr
+    username: str
+    role: UserRole
+    is_varified: bool
+    full_name: str | None = None
+    bio: str | None = None
+    avatar_url: str | None = None
+    website: str | None = None
+    location: str | None = None
+    created_at: datetime
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+class UserProfileUpdate(BaseModel):
+    full_name: str | None = Field(
+        default=None,
+        max_length=100
+    )
+
+    bio: str | None = Field(
+        default=None,
+        max_length=500
+    )
+
+    avatar_url: HttpUrl | None = None
+
+    website: HttpUrl | None = None
+
+    location: str | None = Field(
+        default=None,
+        max_length=100
+    )
+
+class UserProfileResponse(BaseModel):
+
+    username: str
+
+    full_name: str | None = None
+
+    bio: str | None = None
+
+    avatar_url: str | None = None
+
+    website: str | None = None
+
+    location: str | None = None
+
+    role: UserRole
+
+    created_at: datetime
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+    
